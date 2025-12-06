344252c4dddd5f81735f7919ef0733eb8a646de2
IbtissamElhamdaoui <i.elhamdaoui7727@uca.ac.ma>
2025-12-06 18:50:02 +0100

    Ajout exercice1

---
+++ Exercice01.js
@@ -0,0 +1,11 @@
+let x = "150";
+let y = 150;
+let z = true;
+console.log("Type de x :", typeof x);
+console.log("Type de y :", typeof y);
+console.log("Type de z :", typeof z);
+x = Number(x);
+console.log("Nouveau type de x :", typeof x);
+console.log("Valeur de x :", x);
+console.log("\nExplication : JavaScript est un langage à typage dynamique.");
+console.log("Les variables peuvent changer de type durant l'exécution du programme.");
